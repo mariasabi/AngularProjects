0ad395f581df1b0cdf9215d4ea97963d17f0615f
mariasabi <mariasabi16@gmail.com>
2024-08-01 18:22:06 +0530

    integrated with microservices

--- OrderManagement/src/app/api.service.ts
+++ OrderManagement/src/app/api.service.ts
@@ -12,7 +12,7 @@ constructor(private http: HttpClient) { }
 
 async getItems(): Promise<any> {
     try {
-        const data = await this.http.get('https://localhost:7196/api/Order/getItems').toPromise();
+        const data = await this.http.get('https://localhost:7173/api/Order/getItems').toPromise();
         console.log(data);
         return data;
     } catch (error) {
@@ -21,17 +21,17 @@ async getItems(): Promise<any> {
     }
 }
 public loginUser(user:User):Observable<string>{
-return this.http.post('https://localhost:7196/api/User/login',user,
+return this.http.post('https://localhost:7044/api/User/login',user,
   {responseType:'text'}
 );
 }
 public registerUser(user:User):Observable<string>{
-  return this.http.post('https://localhost:7196/api/User/register',user,
+  return this.http.post('https://localhost:7044/api/User/register',user,
     {responseType:'text',}
   );
 }
 public resetUser(user:any):Observable<string>{
-  return this.http.put('https://localhost:7196/api/User/resetPassword',user,
+  return this.http.put('https://localhost:7044/api/User/resetPassword',user,
     {responseType:'text',}
   );
 }
@@ -40,7 +40,7 @@ async updateItems(inputData:ItemData){
   const headers=new HttpHeaders({
       'Content-Type':'application/json'
     })
-     const response=await this.http.put<ItemData>('https://localhost:7196/api/DynamicOrder/updateItemDynamic',inputData, { headers }).toPromise();
+     const response=await this.http.put<ItemData>('https://localhost:7173/api/Order/updateItem',inputData, { headers }).toPromise();
      return response;  
 }
 
@@ -48,12 +48,12 @@ async deleteItem(inputData:number){
   const headers=new HttpHeaders({
       'Content-Type':'application/json'
     })
-    const response=await this.http.delete<number>(`https://localhost:7196/api/Order/deleteItem?id=${inputData}`, { headers }).toPromise();
+    const response=await this.http.delete<number>(`https://localhost:7173/api/Order/deleteItem?id=${inputData}`, { headers }).toPromise();
     return response;
 }
 async getItem(inputData:number): Promise<any> {
     
-        const data = await this.http.get(`https://localhost:7196/api/Order/getItem?id=${inputData}`).toPromise();
+        const data = await this.http.get(`https://localhost:7173/api/Order/getItem?id=${inputData}`).toPromise();
         console.log(data);
         return data;
     
@@ -61,7 +61,7 @@ async getItem(inputData:number): Promise<any> {
 
 async getPageItem(page: number, pageSize: number): Promise<any> {
   try {
-      const data = await this.http.get(`https://localhost:7196/api/Order/getPaginatedItems?page=${page}&pageSize=${pageSize}`).toPromise();
+      const data = await this.http.get(`https://localhost:7173/api/Order/getPaginatedItems?page=${page}&pageSize=${pageSize}`).toPromise();
       console.log(data);
       return data;
   } catch (error) {
@@ -74,14 +74,14 @@ async insertItems(inputData:ItemData){
     const headers=new HttpHeaders({
         'Content-Type':'application/json'
       })
-      const response=await this.http.post<ItemData>('https://localhost:7196/api/Order/addItem',inputData, { headers }).toPromise();
+      const response=await this.http.post<ItemData>('https://localhost:7173/api/Order/addItem',inputData, { headers }).toPromise();
       return response;
 }
 
  async bulkInsert(formData: FormData){
   
   try {
-    const response = await this.http.post<any>('https://localhost:7196/api/DynamicOrder/bulkaddItemsDynamic', formData, {
+    const response = await this.http.post<any>('https://localhost:7173/api/Order/bulkaddItemsDynamic', formData, {
       responseType: 'text' as 'json',
       reportProgress: true,
       observe: 'events'
